25b2e58bfa32949deec62b210d6d50656b391677
tnyima19 <66368294+tnyima19@users.noreply.github.com>
2022-09-27 08:55:53 -0400

    updated method, using left prefix and right suffi

--- productExceptSelf.cpp
+++ productExceptSelf.cpp
@@ -27,6 +27,40 @@ vector<int> productExceptSelf(vector<int>& nums){
         return new_arr;
 }
 
+// Alternate method. 
+vector<int> productExceptSelf2(vector<int>& nums){
+    int left[nums.size()];
+    int right[nums.size()];
+    int total = 1;
+    vector<int> arr;
+    for(int i=0; i<nums.size();++i){
+        if(i==0){
+            left[i]= 1;
+        }else{
+            left[i]= left[i-1]* nums[i-1];
+        }
+        
+    }
+
+    total = 1;
+    for(int j=nums.size()-1;j>=0;--j){
+        if(j==nums.size()-1){
+            right[j]= 1;
+        }else{
+            right[j] = right[j+1] * nums[j+1];
+        }
+    }
+
+
+    for(int i=0;i<nums.size();++i){
+        total = left[i]* right[i];
+        arr.push_back(total);
+    }
+    return arr;
+
+
+}
+
 int main(){
 
     vector<int> arr;
@@ -34,7 +68,7 @@ int main(){
     arr.push_back(2);
     arr.push_back(3);
     arr.push_back(4);
-    arr = productExceptSelf(arr);
+    arr = productExceptSelf2(arr);
 
     for(int i=0;i<arr.size();++i){
         cout<<arr[i]<<endl;
